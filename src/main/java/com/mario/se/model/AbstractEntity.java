287d224603f1eac55abf1584b20709ffb1cd21d0
Mario de Mario Nhanengue <mariopalmira7@gmail.com>
2026-08-01 22:15:28 +0200

    Estrutura

--- src/main/java/com/mario/se/model/AbstractEntity.java
+++ src/main/java/com/mario/se/model/AbstractEntity.java
@@ -1,5 +1,47 @@
 package com.mario.se.model;
 
-public abstract class AbstractEntity {
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
+public abstract class AbstractEntity <ID extends Serializable> implements Serializable{
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity other = (AbstractEntity) obj;
+		return id == other.id;
+	}
+	
+	
 
 }
--- src/main/java/com/mario/se/model/Estudante.java
+++ src/main/java/com/mario/se/model/Estudante.java
@@ -1,5 +1,51 @@
 package com.mario.se.model;
 
-public class Estudante {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="Estudante")
+public class Estudante extends AbstractEntity<Long> {
+
+	
+	
+	@Column(length = 50)
+	   private String nome;
+	
+	@Column(length = 50)
+   private String email;
+
+	
+	
+	public Estudante(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+   
+	
+	   
 }

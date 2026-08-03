90b22f984d25bdafcb318067fc80f2c69fd5a781
Mario de Mario Nhanengue <mariopalmira7@gmail.com>
2026-08-03 19:28:41 +0200

    actualizando a classe cursos no model

--- src/main/java/com/mario/se/model/Curso.java
+++ src/main/java/com/mario/se/model/Curso.java
@@ -3,7 +3,6 @@ package com.mario.se.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
